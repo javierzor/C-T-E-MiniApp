9acce75ec1c40c588c7bb65197a209e445dd4c2b
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2023-02-14 18:41:00 -0600

    CambiO se genera el objecto dataTOM con todas las variables, undefined o null, son datos vacios.

--- src/app/crear-ejercicio/crear-ejercicio.page.ts
+++ src/app/crear-ejercicio/crear-ejercicio.page.ts
@@ -34,7 +34,7 @@ export class CrearEjercicioPage implements OnInit {
   carga: any;
 
   valorCalculado: number;
-  metros: number = -1;
+  metros: number = null;
   m2calculado: number;
   datosTOM: {
     tarea: number;
@@ -45,6 +45,13 @@ export class CrearEjercicioPage implements OnInit {
     balones: number;
     interrupciones: number;
     metros: number;
+
+    m2?: {
+      jugadores?: number;
+      lado1?: number;
+      lado2?: number;
+      m2jugador?: number;
+    };
   };
   constructor(
     private location: Location,
@@ -90,13 +97,18 @@ export class CrearEjercicioPage implements OnInit {
 
   recalculadodeM2PORjug(
     m2jugadores: number | string,
-    m2lad1: number | string,
+    m2lado1: number | string,
     m2lado2: number | string
   ) {
     console.log('recalculando metros al cuadrado por jugador');
-    if (m2jugadores == 0 || m2lad1 == 0 || m2lado2 == 0) return;
+    if (!this.datosTOM.m2) this.datosTOM.m2 = {};
+    this.datosTOM.m2.lado1 = m2lado1 as number;
+    this.datosTOM.m2.lado2 = m2lado2 as number;
+    this.datosTOM.m2.jugadores = m2jugadores as number;
+
+    if (m2jugadores == 0 || m2lado1 == 0 || m2lado2 == 0) return;
     this.m2calculado =
-      ((m2lad1 as number) * (m2lado2 as number)) / (m2jugadores as number);
+      ((m2lado1 as number) * (m2lado2 as number)) / (m2jugadores as number);
     if (this.m2calculado < 40) {
       this.mostrarmetroscuadradosporjugador = '<40';
       this.metros = 0;
@@ -117,6 +129,7 @@ export class CrearEjercicioPage implements OnInit {
       this.mostrarmetroscuadradosporjugador = '>40';
       this.metros = 3;
     }
+    this.datosTOM.m2.m2jugador = this.m2calculado;
   }
 
   ONCHANGETOTALMANUALTEMPORAL(event) {
@@ -175,10 +188,6 @@ export class CrearEjercicioPage implements OnInit {
     }
   }
   async guardar() {
-    if (!this.valorCalculado) {
-      console.log('Falta llenar los datos del ejercicio');
-      return;
-    }
     var data = {
       nombre_solicitud: 'guardarejercicio',
       id_sesion: this.quizzseleccionadoquellega.id_sesion,
@@ -235,14 +244,24 @@ export class CrearEjercicioPage implements OnInit {
     balones: string,
     interrupciones: string
   ) {
-    /*  console.log('tarea:', tarea);
+    console.log('tarea:', tarea);
     console.log('jugadores:', jugadores);
     console.log('equipos:', parseInt(equipos));
     console.log('nivel:', nivel);
     console.log('intesidad:', intensidad);
     console.log('metros:', this.metros);
     console.log('balones:', balones);
-    console.log('interrupciones:', interrupciones); */
+    console.log('interrupciones:', interrupciones);
+    this.datosTOM = {
+      tarea: parseInt(tarea),
+      equipos: parseInt(equipos),
+      jugadores: parseInt(jugadores),
+      nivel: parseInt(nivel),
+      intensidad: parseInt(intensidad),
+      metros: this.metros,
+      balones: parseInt(balones),
+      interrupciones: parseInt(interrupciones),
+    };
     if (
       tarea &&
       jugadores &&
@@ -253,16 +272,6 @@ export class CrearEjercicioPage implements OnInit {
       balones &&
       interrupciones
     ) {
-      this.datosTOM = {
-        tarea: parseInt(tarea),
-        equipos: parseInt(equipos),
-        jugadores: parseInt(jugadores),
-        nivel: parseInt(nivel),
-        intensidad: parseInt(intensidad),
-        metros: this.metros,
-        balones: parseInt(balones),
-        interrupciones: parseInt(interrupciones),
-      };
       console.log('Valores Correctos');
       this.valorCalculado =
         parseInt(tarea) +
